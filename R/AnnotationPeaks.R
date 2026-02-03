51a8a8e112a9e8e539276ef6d8e77ba085865b8b
r-blanchet <79264525+r-blanchet@users.noreply.github.com>
2026-02-03 15:20:56 +0100

    Update AnnotationPeaks.R
    Including JASPAR 2024 to the possible motif annotations sources

--- R/AnnotationPeaks.R
+++ R/AnnotationPeaks.R
@@ -423,7 +423,16 @@ addMotifAnnotations <- function(
 
   .logDiffTime(paste0("Getting Motif Set, Species : ", species), t1 = tstart, verbose = TRUE, logFile = logFile)
 
-  if(tolower(motifSet)=="jaspar2020"){
+  if(tolower(motifSet)=="jaspar2024"){
+    
+    .requirePackage("JASPAR2024",installInfo='BiocManager::install("JASPAR2024")')
+    args <- list(species = species, collection = collection, ...)
+    motifs <- TFBSTools::getMatrixSet(JASPAR2024::JASPAR2024, args)
+    obj <- .summarizeJASPARMotifs(motifs)
+    motifs <- obj$motifs
+    motifSummary <- obj$motifSummary
+
+  }else if(tolower(motifSet)=="jaspar2020"){
     
     .requirePackage("JASPAR2020",installInfo='BiocManager::install("JASPAR2020")')
     args <- list(species = species, collection = collection, ...)
@@ -443,7 +452,7 @@ addMotifAnnotations <- function(
 
   }else if(tolower(motifSet)=="jaspar2016"){
 
-    .requirePackage("JASPAR2016",installInfo='BiocManager::install("JASPAR2018")')
+    .requirePackage("JASPAR2016",installInfo='BiocManager::install("JASPAR2016")')
     args <- list(species = species, collection = collection, ...)
     motifs <- TFBSTools::getMatrixSet(JASPAR2016::JASPAR2016, args)
     obj <- .summarizeJASPARMotifs(motifs)
